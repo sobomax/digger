639316e40b6816f486ba72c6b9f8b4cefaf5559f
Maksym Sobolyev <sobomax@sippysoft.com>
2014-08-27 03:07:08 -0700

    Make this working with SDL 2.0 properly. The main difference is that we need to explicitly silence-out the buffer in the callback. Dending on the format, it means filling it with some non-zero data, 127 for U8 in particular.

--- sdl_snd.c
+++ sdl_snd.c
@@ -8,9 +8,6 @@
 void fill_audio(void *udata, uint8_t *stream, int len);
 uint8_t getsample(void);
 
-uint8_t *buf;
-uint16_t bsize;
-
 bool wave_device_available = false;
 
 bool initsounddevice(void)
@@ -18,16 +15,31 @@ bool initsounddevice(void)
 	return(true);
 }
 
+struct sudata {
+    SDL_AudioSpec obtained;
+    uint8_t *buf;
+    uint16_t bsize;
+};
+
 bool setsounddevice(int base, int irq, int dma, uint16_t samprate, uint16_t bufsize)
 {
 	SDL_AudioSpec wanted;
+        struct sudata *sud;
 	bool result = false;
 	
+        sud = malloc(sizeof(*sud));
+        if (sud == NULL) {
+                fprintf(stderr, "setsounddevice: malloc(3) failed\n");
+                return (false);
+        }
+        memset(sud, '\0', sizeof(*sud));
+        SDL_zero(wanted);
+        SDL_zero(sud->obtained);
 	wanted.freq = samprate;
 	wanted.samples = bufsize;
 	wanted.channels = 1;
 	wanted.format = AUDIO_U8;
-	wanted.userdata = NULL;
+	wanted.userdata = sud;
 	wanted.callback = fill_audio;
 
 #ifdef _VGL
@@ -35,15 +47,22 @@ bool setsounddevice(int base, int irq, int dma, uint16_t samprate, uint16_t bufs
 #endif
 
 	if ((SDL_Init(SDL_INIT_AUDIO)) >= 0)
-		if ((SDL_OpenAudio(&wanted, NULL)) >= 0)
+		if ((SDL_OpenAudio(&wanted, &sud->obtained)) >= 0)
 			result = true;
-	if (result == false)
+	if (result == false) {
 		fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
-	else {
-		buf = malloc(bufsize);
-		bsize = bufsize;
-		wave_device_available = true;
-	}
+                free(sud);
+                return (false);
+        }
+        sud->bsize = sud->obtained.size;
+	sud->buf = malloc(sud->bsize);
+        if (sud->buf == NULL) {
+                fprintf(stderr, "setsounddevice: malloc(3) failed\n");
+                SDL_CloseAudio();
+                free(sud);
+                return (false);
+        }
+	wave_device_available = true;
 
 #ifdef _VGL
 	initkeyb();
@@ -55,13 +74,19 @@ bool setsounddevice(int base, int irq, int dma, uint16_t samprate, uint16_t bufs
 void fill_audio(void *udata, uint8_t *stream, int len)
 {
 	int i;
+        struct sudata *sud;
 
-	if (len > bsize)
-		len = bsize;
+        sud = (struct sudata *)udata;
+        SDL_memset(stream, sud->obtained.silence, len);
+	if (len > sud->bsize) {
+                fprintf(stderr, "fill_audio: OUCH, len > bsize!\n");
+		len = sud->bsize;
+        }
 	for(i = 0; i<len; i++)
-		buf[i] = getsample();
+		sud->buf[i] = getsample();
 
-	SDL_MixAudio(stream, buf, len, SDL_MIX_MAXVOLUME);
+	SDL_MixAudioFormat(stream, sud->buf, sud->obtained.format, len,
+            SDL_MIX_MAXVOLUME / 2);
 }
 
 
