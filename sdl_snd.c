09386ed114088015779d89eb1523d4cd6e7c7db6
Maksym Sobolyev <sobomax@sippysoft.com>
2014-08-27 13:10:27 -0700

    Filter out sound via first order low pass recursive filter with the cutoff frequency of 5kHz.

--- sdl_snd.c
+++ sdl_snd.c
@@ -4,6 +4,7 @@
 #include "def.h"
 #include "device.h"
 #include "hardware.h"
+#include "digger_math.h"
 
 void fill_audio(void *udata, uint8_t *stream, int len);
 uint8_t getsample(void);
@@ -15,11 +16,47 @@ bool initsounddevice(void)
 	return(true);
 }
 
+struct fo_filter {
+    double a;
+    double b;
+    double z0;
+    double z1;
+};
+
 struct sudata {
     SDL_AudioSpec obtained;
     uint8_t *buf;
     uint16_t bsize;
+    struct fo_filter *fltr;
 };
+
+static struct fo_filter *
+fo_init(double Fs, double Fc)
+{
+        struct fo_filter *fofp;
+        double n, w;
+
+        fofp = malloc(sizeof(*fofp));
+        memset(fofp, '\0', sizeof(*fofp));
+        if (Fs < Fc * 2.0) {
+                fprintf(stderr, "fo_init: cutoff frequency (%f) should be less "
+                    "than half of the sampling rate (%f)\n", Fc, Fs);
+                abort();
+        }
+        w = tan(D_PI * Fc / Fs);
+        n = 1.0 / (1.0 + w);
+        fofp->a = n * (w - 1);
+        fofp->b = n * w;
+        return (fofp);
+}
+
+static double
+fo_apply(struct fo_filter *fofp, double x)
+{
+        fofp->z1 = (x * fofp->b) + (fofp->z0 * fofp->b) - (fofp->z1 * fofp->a);
+        fofp->z0 = x;
+        return (fofp->z1);
+}
 
 bool setsounddevice(int base, int irq, int dma, uint16_t samprate, uint16_t bufsize)
 {
@@ -62,6 +99,9 @@ bool setsounddevice(int base, int irq, int dma, uint16_t samprate, uint16_t bufs
                 free(sud);
                 return (false);
         }
+        sud->fltr = fo_init(sud->obtained.freq, 5000);
+        sud->fltr->z0 = sud->obtained.silence;
+        sud->fltr->z1 = sud->obtained.silence;
 	wave_device_available = true;
 
 #ifdef _VGL
@@ -82,8 +122,9 @@ void fill_audio(void *udata, uint8_t *stream, int len)
                 fprintf(stderr, "fill_audio: OUCH, len > bsize!\n");
 		len = sud->bsize;
         }
-	for(i = 0; i<len; i++)
-		sud->buf[i] = getsample();
+	for (i = 0; i < len; i++) {
+		sud->buf[i] = fo_apply(sud->fltr, getsample());
+        }
 
 	SDL_MixAudioFormat(stream, sud->buf, sud->obtained.format, len,
             SDL_MIX_MAXVOLUME / 2);
