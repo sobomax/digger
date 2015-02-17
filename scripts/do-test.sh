75876e6e69636c4c311d6c7b42a9b9e92e51c386
Maksym Sobolyev <sobomax@sippysoft.com>
2015-02-16 18:58:32 -0800

    Run `sdl2-config' just to see what it outputs.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -7,6 +7,7 @@ ${CC} --version
 sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots
 sudo apt-get update -qq
 sudo apt-get install -qq libsdl2-dev
+sdl2-config --cflags
 make ARCH=LINUX all
 make clean
 make ARCH=mingw all
