720f3ceef88217cd053467b1e7d7a27e3da0babf
Maksym Sobolyev <sobomax@gmail.com>
2015-02-16 18:07:54 -0800

    Use some debian black magic to get libsdl2 installed.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -4,7 +4,9 @@ set -e
 
 uname -a
 ${CC} --version
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libsdl2-dev
+sudo -c "echo 'deb http://http.debian.net/debian wheezy-backports main' > /etc/apt/sources.list.d/wb.list"
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -t wheezy-backports install libsdl2-dev
 make ARCH=LINUX all
 make clean
 make ARCH=mingw all
