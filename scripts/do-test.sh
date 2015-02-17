cacecea5e623af917a577e5bf7c8e202ed71f473
Maksym Sobolyev <sobomax@gmail.com>
2015-02-16 17:57:48 -0800

    Fix apt-get invocation.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -4,7 +4,7 @@ set -e
 
 uname -a
 ${CC} --version
-sudo apt-get DEBIAN_FRONTEND=noninteractive install libsdl2-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get install libsdl2-dev
 make ARCH=LINUX all
 make clean
 make ARCH=mingw all
