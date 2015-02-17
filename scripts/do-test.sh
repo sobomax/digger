0251f2ecf21abb24e79f69b385d4677ceac4610d
Maksym Sobolyev <sobomax@sippysoft.com>
2015-02-16 18:42:06 -0800

    Install key needed for the wheezy-backports.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -5,6 +5,8 @@ set -e
 uname -a
 ${CC} --version
 sudo sh -c "echo 'deb http://http.debian.net/debian wheezy-backports main' > /etc/apt/sources.list.d/wb.list"
+gpg --keyserver pgpkeys.mit.edu --recv-key 8B48AD6246925553
+gpg -a --export 8B48AD6246925553 | sudo apt-key add -
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -t wheezy-backports install libsdl2-dev
 make ARCH=LINUX all
