8635554953b9100333a879ef9ee4de9f4e211585
Maksym Sobolyev <sobomax@gmail.com>
2015-02-16 19:31:00 -0800

    Also fetch/unpack binary dependencies for the windows build.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -8,8 +8,16 @@ sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots
 sudo apt-get update -qq
 sudo apt-get remove -qq -y mingw32
 sudo apt-get install -qq libsdl2-dev mingw-w64
-sdl2-config --cflags
+mkdir deps
+cd deps
+wget https://www.libsdl.org/release/SDL2-devel-2.0.3-mingw.tar.gz
+tar xfz SDL2-devel-2.0.3-mingw.tar.gz
+wget http://zlib.net/zlib128-dll.zip
+mkdir zlib-1.2.8
+cd zlib-1.2.8
+unzip ../zlib128-dll.zip
+cd ../..
+
 make ARCH=LINUX all
-make ARCH=LINUX clean
-sudo apt-get install -qq mingw-w64
-make ARCH=MINGW all
+make ARCH=MINGW clean
+make ARCH=MINGW MINGW_DEPS_ROOT=`pwd`/deps all
