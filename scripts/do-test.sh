fbd5d99135bc47d58a945b7b4eae7f2e421df6f5
Maksym Sobolyev <sobomax@sippysoft.com>
2015-02-16 18:48:48 -0800

    Try different way to install libsdl2-dev.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -4,11 +4,8 @@ set -e
 
 uname -a
 ${CC} --version
-sudo sh -c "echo 'deb http://http.debian.net/debian wheezy-backports main' > /etc/apt/sources.list.d/wb.list"
-gpg --keyserver pgpkeys.mit.edu --recv-key 8B48AD6246925553
-gpg -a --export 8B48AD6246925553 | sudo apt-key add -
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -t wheezy-backports install libpulse-dev libsdl2-dev
+sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshot
+sudo apt-get install -qq libsdl2-dev
 make ARCH=LINUX all
 make clean
 make ARCH=mingw all
