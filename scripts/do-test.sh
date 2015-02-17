a26f5b0256b864530ea879fcf5e4b9647e9fbcc1
Maksym Sobolyev <sobomax@gmail.com>
2015-02-16 18:10:20 -0800

    Try harder to run sheel subroutine under sudo.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -4,7 +4,7 @@ set -e
 
 uname -a
 ${CC} --version
-sudo -c "echo 'deb http://http.debian.net/debian wheezy-backports main' > /etc/apt/sources.list.d/wb.list"
+sudo sh -c "echo 'deb http://http.debian.net/debian wheezy-backports main' > /etc/apt/sources.list.d/wb.list"
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -t wheezy-backports install libsdl2-dev
 make ARCH=LINUX all
