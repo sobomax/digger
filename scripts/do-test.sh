5238badd2875d0f50f29fd302d8414ed15e06a6d
Maksym Sobolyev <sobomax@sippysoft.com>
2015-02-16 18:51:37 -0800

    Also run apt-get update.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -5,6 +5,7 @@ set -e
 uname -a
 ${CC} --version
 sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshot
+sudo apt-get update -qq
 sudo apt-get install -qq libsdl2-dev
 make ARCH=LINUX all
 make clean
