f31955ea57c82ad13621dd96f44c3341410e45c0
Maksym Sobolyev <sobomax@sippysoft.com>
2015-02-16 18:55:13 -0800

    Fix type: snapshot -> snapshots.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -4,7 +4,7 @@ set -e
 
 uname -a
 ${CC} --version
-sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshot
+sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots
 sudo apt-get update -qq
 sudo apt-get install -qq libsdl2-dev
 make ARCH=LINUX all
