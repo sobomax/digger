40eaa13c8f579ed83251256a3f5ccf5123ea44d5
Maksym Sobolyev <sobomax@sippysoft.com>
2017-09-11 14:14:56 -0700

    Read .ini file before we process command line options so that it's possible to override.

--- main.c
+++ main.c
@@ -319,7 +319,6 @@ void maininit(void)
   setretr(true);
   initkeyb();
   detectjoy();
-  inir();
   initsound();
   recstart();
   maininited = 1;
@@ -329,6 +328,7 @@ int main(int argc,char *argv[])
 {
   int rval;
 
+  inir();
   parsecmd(argc,argv);
   maininit();
   rval = mainprog();
