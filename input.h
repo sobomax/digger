39271117d4fa35fe254aa4c17d40526083948dc0
Maksym Sobolyev <sobomax@sippysoft.com>
2017-09-12 15:07:20 -0700

    Replace hardcoded key offsets with symbolic values.

--- input.h
+++ input.h
@@ -16,6 +16,16 @@ extern int16_t akeypressed;
 
 #define NKEYS 19
 
+#define DKEY_CHT 10 /* Cheat */
+#define DKEY_SUP 11 /* Increase speed */
+#define DKEY_SDN 12 /* Decrease speed */
+#define DKEY_MTG 13 /* Toggle music */
+#define DKEY_STG 14 /* Toggle sound */
+#define DKEY_EXT 15 /* Exit */
+#define DKEY_PUS 16 /* Pause */
+#define DKEY_MCH 17 /* Mode change */
+#define DKEY_SDR 18 /* Save DRF */
+
 extern int keycodes[NKEYS][5];
 extern bool krdf[NKEYS];
 extern bool pausef,mode_change;
