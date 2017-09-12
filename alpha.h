46ff13fc4f71ae4134ad48e72d3953df8128ad7d
Maksym Sobolyev <sobomax@sippysoft.com>
2017-09-12 15:42:19 -0700

    Use isvalchar() instead of rolling out own range check.

--- alpha.h
+++ alpha.h
@@ -1,6 +1,6 @@
 /* Digger Remastered
    Copyright (c) Andrew Jenner 1998-2004 */
 
-extern const uint8_t const *ascii2vga[];
+extern const uint8_t * const ascii2vga[];
 
 #define isvalchar(ch) ((((ch) - 32) < 0x5f) && ((ch) > 32) && ascii2vga[(ch) - 32] != NULL)
