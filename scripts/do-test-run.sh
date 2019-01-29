203fb185ac78225f045be20ba130e4054a664c83
Maksym Sobolyev <sobomax@sippysoft.com>
2019-01-28 17:59:30 -0800

    Set \Q option so that there is no delay between levels.

--- scripts/do-test-run.sh
+++ scripts/do-test-run.sh
@@ -6,5 +6,5 @@ mv ./production/* ./
 for x in tests/data/*.drf
 do
   echo -n `basename ${x}`": "
-  SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger /S:0 /E:${x}
+  SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger /Q /S:0 /E:${x}
 done
