2847effc7972af41a91a4c0aab5637643bfc5e1a
Maksym Sobolyev <sobomax@sippysoft.com>
2019-01-31 13:43:35 -0800

    If the test file is less than 6kb run it with sound "on". This allows testing interaction between soundgen and main threads without incurring huge time penalty that due to the time it needs to play "RIP" and "level up" musics.

--- scripts/do-test-run.sh
+++ scripts/do-test-run.sh
@@ -5,12 +5,26 @@ set -e
 DIFF="diff -u"
 
 mv ./production/* ./
-for x in tests/data/*.drf
+for TTYPE in short long
 do
-  TFNAME="`basename ${x}`"
-  TRFNAME="${TFNAME}.out"
-  echo -n "${TFNAME}: "
-  SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger /Q /S:0 /E:${x} > "${TRFNAME}" 2>/dev/null
-  ${DIFF} "tests/results/${TRFNAME}" "${TRFNAME}"
-  echo "PASS"
+  for x in tests/data/*.drf
+  do
+    DIG_OPTS="/S:0 /E:${x}"
+    if [ "${TTYPE}" = "long" ]
+    then
+      TSIZE=`du -k ${x} | awk '{print $1}'`
+      if [ ${TSIZE} -gt 5 ]
+      then
+	continue
+      fi
+    else
+      DIG_OPTS="/Q ${DIG_OPTS}"
+    fi
+    TFNAME="`basename ${x}`"
+    TRFNAME="${TFNAME}.out"
+    echo -n "${TFNAME} (${TTYPE}): "
+    SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger ${DIG_OPTS} > "${TRFNAME}" 2>/dev/null
+    ${DIFF} "tests/results/${TRFNAME}" "${TRFNAME}"
+    echo "PASS"
+  done
 done
