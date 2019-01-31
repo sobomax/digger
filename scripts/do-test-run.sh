16033d5625481cce236392d6c2a1aeeb38590181
Maksym Sobolyev <sobomax@sippysoft.com>
2019-01-31 12:02:55 -0800

    Validate test run results against known good output.

--- scripts/do-test-run.sh
+++ scripts/do-test-run.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+DIFF="diff -u"
+
 mv ./production/* ./
 for x in tests/data/*.drf
 do
-  echo -n `basename ${x}`": "
-  SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger /Q /S:0 /E:${x}
+  TFNAME="`basename ${x}`"
+  TRFNAME="${TFNAME}.out"
+  echo -n "${TFNAME}: "
+  SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger /Q /S:0 /E:${x} > "${TRFNAME}" 2>/dev/null
+  ${DIFF} "tests/results/${TRFNAME}" "${TRFNAME}"
+  echo "PASS"
 done
