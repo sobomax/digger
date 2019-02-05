deba0cf859ea282ef3efdb3d02f51789c9e9a317
Maksym Sobolyev <sobomax@sippysoft.com>
2019-02-05 13:22:41 -0800

    Add xlong run.

--- scripts/do-test-run.sh
+++ scripts/do-test-run.sh
@@ -11,6 +11,8 @@ do
   do
     DIG_OPTS="/E:${x}"
     DIG_OPT_FSPD="/S:0"
+    TFNAME="`basename ${x}`"
+    TRFNAME="${TFNAME}.out"
     if [ "${TTYPE}" = "long" -o "${TTYPE}" = "xlong" ]
     then
       TSIZE=`du -k ${x} | awk '{print $1}'`
@@ -22,11 +24,17 @@ do
       then
 	DIG_OPTS="${DIG_OPT_FSPD} ${DIG_OPTS}"
       fi
+      if [ "${TTYPE}" = "xlong" ]
+      then
+        eval `cat tests/results/${TRFNAME}`
+	if [ ${frames} -gt 7000 ]
+        then
+          continue
+	fi
+      fi
     else
       DIG_OPTS="/Q ${DIG_OPT_FSPD} ${DIG_OPTS}"
     fi
-    TFNAME="`basename ${x}`"
-    TRFNAME="${TFNAME}.out"
     echo -n "${TFNAME} (${TTYPE}): "
     SDL_AUDIODRIVER=dummy SDL_VIDEODRIVER=dummy DIGGER_CI_RUN=1 ./digger ${DIG_OPTS} > "${TRFNAME}" 2>/dev/null
     ${DIFF} "tests/results/${TRFNAME}" "${TRFNAME}"
