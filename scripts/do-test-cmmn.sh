559d33444a82e6ff832394cc2b7add3ef447c208
Maksym Sobolyev <sobomax@sippysoft.com>
2019-02-13 12:09:19 -0800

    We now have libsdl2-dev package in the main repo, use it.

--- scripts/do-test-cmmn.sh
+++ scripts/do-test-cmmn.sh
@@ -4,8 +4,8 @@ set -e
 
 uname -a
 ${CC} --version
-sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots
-sudo apt-get update -qq -y
-sudo apt-get install -qq -y libsdl2-dev
+#sudo apt-add-repository --yes ppa:zoogie/sdl2-snapshots
+#sudo apt-get update -qq -y
+#sudo apt-get install -qq -y libsdl2-dev
 
 mkdir debug production
