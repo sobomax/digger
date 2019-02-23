0e69c81245879b0bc11ade25817faf23b98df83e
Maksym Sobolyev <sobomax@sippysoft.com>
2019-02-23 14:01:10 -0800

    Add dummy GetINIIntDoc() allowing to attach small documentation to the option(s).

--- ini.h
+++ ini.h
@@ -6,6 +6,9 @@
 void WriteINIString(const char *section,const char *key,const char *value,const char *filename);
 void GetINIString(const char *section,const char *key,const char*def,char*dest,int destsize,
                   char *filename);
+
+#define GetINIIntDoc(s,k,de,f,do) GetINIInt((s),(k),(de),(f))
+
 int32_t GetINIInt(const char *section,const char*key,int32_t def,const char*filename);
 void WriteINIInt(const char *section,const char*key,int32_t value,const char*filename);
 bool GetINIBool(const char *section,const char*key,bool def,const char*filename);
